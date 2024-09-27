185f09c283c9c336338a63e0d2edeaadf427175b
chenqi99 <chenqi99@nancal.com>
2024-09-27 12:59:27 +0800

    init

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,10 +1,25 @@
 import { expect, test } from "@playwright/test";
 import { auto } from "../src/auto";
+import {StepOptions} from "../src/types";
 
-const options = {
-  openaiApiKey: 'sk-Gl1V3nAzyE3m8wAiEEx6ijlcoux3Vx5lP4ZVj5v0tmaJYKhe',
-  openaiBaseUrl: 'https://api.openai-proxy.org/v1'
+let nodeenv= process.env.NODE_ENV
+
+if(nodeenv !== 'prod'){
+  require('dotenv').config({path:'./.env.dev'})
 }
+
+const apiKey = process.env.AZURE_OPENAI_KEY;
+const apiVersion = process.env.AZURE_OPENAI_VERSION;
+const resource = process.env.AZURE_OPENAI_RESOURCE;
+const model = process.env.AZURE_OPENAI_MODEL;
+
+const options: StepOptions = {
+  model: model,
+  openaiApiKey: apiKey,
+  openaiBaseUrl: `https://${resource}.openai.azure.com/openai/deployments/${model}`,
+  openaiDefaultQuery: { 'api-version': apiVersion },
+  openaiDefaultHeaders: { 'api-key': apiKey }
+};
 
 test("executes click", async ({ page }) => {
   await page.goto("/");
